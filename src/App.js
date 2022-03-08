87306bb52654b6f4f436324935ccfe43403f6e7f
Fer Adalid <fadalid97@gmail.com>
2022-03-07 18:53:09 -0800

    Contador en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[counter, setCounter] = useState(0)
+console.log('holi')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contador {counter}</h1>
+      <button onClick={() => setCounter(counter+1)}>Sumar</button>
+      <button onClick={() => setCounter(counter - counter)}>Reset</button>
+      <button onClick={() => setCounter(counter-1)}>Restar</button>
     </div>
   );
 }
